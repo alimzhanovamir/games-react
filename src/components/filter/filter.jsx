3eafa2d25892eea373906d07a39d40ca0975deaf
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-04-11 18:15:20 +0300

    Add Filter component template

---
+++ src/components/filter/filter.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import cls from './filter.module.scss';
+
+function Filter() {
+	const countElementsOnPage = [10, 30, 50];
+
+	return (
+		<form className={cls['filter']}>
+			<input className={cls['filter__search']} type="text" placeholder='Поиск'/>
+			<select className={cls['filter__number']} name="" id="">
+				{countElementsOnPage.map( value => <option value={value}>{value}</option> )}
+			</select>
+		</form>
+	)
+}
+
+export default Filter
