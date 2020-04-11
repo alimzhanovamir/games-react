ca2dde9466239c8bce4902e3b701e5070f175e5b
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-04-11 23:41:14 +0300

    App: refactoring for search; Create all games store

--- src/App.js
+++ src/App.js
@@ -2,45 +2,50 @@ import React, { useState, useEffect } from 'react';
 import './App.scss';
 import Layout from "./layout/layout";
 
+export const GamesContext = React.createContext();
+
 function App() {
 
-  const [state, setState] = useState(false);
-  const [favoriteGames, setFavoriteGames] = useState([]);
-  const favoritesRandomGames = [];
-
-  // function getRandomGame(min, max) {
-  //   return Math.random() * (max - min) + min;
-  // }
+  const [state, setState] = useState({});
+  const [loadedFlag, setLoadedFlag] = useState(false);
+  const randomTopGames = [];
 
   useEffect(()=> {
     fetch('https://www.gbchip.com/api/v1/games')
       .then(res => {  return res.json() })
       .then(res => {
+        console.log('get')
         for (let i = 0; i < 5; i++) {
           let x = (Math.random() * res.games.length).toFixed(0);
-          favoritesRandomGames.push( res.games[x] )
+          randomTopGames.push( res.games[x] )
         }
-        console.log(favoritesRandomGames)
-        setFavoriteGames(favoritesRandomGames);
 
-        const games = res.games.filter( game => (
-          favoritesRandomGames.some( someGame => someGame.Name.en !== game.Name.en )
-        )).sort( (a, b) => a.Name.en.localeCompare(b.Name.en) );
+        // Массив игр без тех что в топе
+        const games = res.games.filter( game => {
+          return randomTopGames.some(someGame => someGame.Name.en !== game.Name.en)
+        }).sort( (a, b) => a.Name.en.localeCompare(b.Name.en) ).slice(0,100);
 
-        setTimeout(()=> setState(games), 1000);
-      })
+        setState({
+          allGames: res.games,
+          topGames: [...randomTopGames],
+          games,
+          setState
+        });
+        setLoadedFlag(true);
+      });
   }, []);
 
-  useEffect(()=> {
-    console.log(state)
-  },[state]);
+  // useEffect(()=> {
+  //   if (state) {
+  //     setLoadedFlag(true);
+  //   }
+  // },[state]);
 
-  useEffect(()=> {
-    console.log(favoritesRandomGames)
-  },[favoriteGames]);
-
+  console.log('render App');
   return (
-    <Layout state={state} favoriteGames={favoriteGames}/>
+    <GamesContext.Provider value={state}>
+      <Layout loadedFlag={loadedFlag}/>
+    </GamesContext.Provider>
   );
 
 }
