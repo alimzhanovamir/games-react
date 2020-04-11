30f1750688731935611fc40375eaa57db859f76d
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-04-11 18:15:01 +0300

    Add Header component template

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import cls from './header.module.scss';
+import Filter from "../filter/filter";
 
 function Header() {
 
 	return (
 		<header className={cls['header']}>
 			<h1 className={cls['header__title']}>Игры</h1>
+			<Filter/>
 		</header>
 	)
 }
