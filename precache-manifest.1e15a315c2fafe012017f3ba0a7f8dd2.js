0e5fac02df7e6c20eec0f642e386415d10bf0cd7
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-04-11 20:10:47 +0300

    Updates

---
+++ precache-manifest.1e15a315c2fafe012017f3ba0a7f8dd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adfc3c5f0a32d60bdf670187db7eb1eb",
+    "url": "/egamings-test-react/index.html"
+  },
+  {
+    "revision": "1a40868a4f74dd3cd9ca",
+    "url": "/egamings-test-react/static/css/main.65d125ed.chunk.css"
+  },
+  {
+    "revision": "bb45a9da90f38f8e0322",
+    "url": "/egamings-test-react/static/js/2.3f0657e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/egamings-test-react/static/js/2.3f0657e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a40868a4f74dd3cd9ca",
+    "url": "/egamings-test-react/static/js/main.a7e9fb32.chunk.js"
+  },
+  {
+    "revision": "f08d7f66323d07e4f7cf",
+    "url": "/egamings-test-react/static/js/runtime-main.e0772d0c.js"
+  }
+]);
